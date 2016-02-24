f3f11538b44a9b3c8c830287fe5fbdd6f1d2cf71
Tiffany <tet226@nyu.edu>
2016-02-23 16:50:00 -0800

    comment out watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,26 +55,26 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
 
-  grunt.registerTask('server-dev', function (target) {
-    // Running nodejs in a different process and displaying output on the main console
-    var nodemon = grunt.util.spawn({
-      cmd: 'grunt',
-      grunt: true,
-      args: 'nodemon'
-    });
-    nodemon.stdout.pipe(process.stdout);
-    nodemon.stderr.pipe(process.stderr);
+  // grunt.registerTask('server-dev', function (target) {
+  //   // Running nodejs in a different process and displaying output on the main console
+  //   var nodemon = grunt.util.spawn({
+  //     cmd: 'grunt',
+  //     grunt: true,
+  //     args: 'nodemon'
+  //   });
+  //   nodemon.stdout.pipe(process.stdout);
+  //   nodemon.stderr.pipe(process.stderr);
 
-    grunt.task.run([ 'watch' ]);
-  });
+  //   grunt.task.run([ 'watch' ]);
+  // });
 
 
-  grunt.registerTask('upload', function(n) {
-    if (grunt.option('prod')) {
-      // add your production server task here
-    }
-    grunt.task.run([ 'server-dev' ]);
-  });
+  // grunt.registerTask('upload', function(n) {
+  //   if (grunt.option('prod')) {
+  //     // add your production server task here
+  //   }
+  //   grunt.task.run([ 'server-dev' ]);
+  // });
 
   ////////////////////////////////////////////////////
   // Main grunt tasks
